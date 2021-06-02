7ef3a8753865327760c61af82592740e2c809760
Arika Turing <ablenker@gmail.com>
2021-06-02 15:26:45 -0600

    Add tea validations in spec and model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,11 @@
 class Tea < ApplicationRecord
   belongs_to :subscription
   has_many :customers, through: :subscription
+
+  validates_presence_of :title,
+                        :description,
+                        :brew_time
+
+  validates :brew_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Tea, type: :model do
     it { should belong_to :subscription }
     it { should have_many(:customers).through(:subscription) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :brew_time }
+    it { should validate_numericality_of(:brew_time).is_greater_than_or_equal_to(0) }
+  end
 end
