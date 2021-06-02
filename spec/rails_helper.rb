5c4fd8b49e1d28a75f07b0db1c7974f39bef47ff
Arika Turing <ablenker@gmail.com>
2021-06-02 11:56:06 -0600

    Add testing info to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+SimpleCov.add_filter ['spec', 'config', '/app/mailers', 'app/jobs', 'app/channels']
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -61,4 +64,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+  config.include RequestRailsHelper, type: :request
+  config.include RequestRailsHelper, type: :model
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+     with.test_framework :rspec
+     with.library :rails
+  end
 end
