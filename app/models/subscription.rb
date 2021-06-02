1469adc68cb82e01e07388394b8322aa2dc4a499
Arika Turing <ablenker@gmail.com>
2021-06-02 14:44:28 -0600

    Add validations to subscription spec and model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,13 @@
 class Subscription < ApplicationRecord
-  belongs_to :customer 
+  belongs_to :customer
   has_many :teas
+
+  validates_presence_of :title,
+                        :status,
+                        :frequency
+
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  enum status: [:pending, :active, :cancelled]
+  enum frequency: [:two_weeks, :four_weeks, :six_weeks]
 end
