82f20a687f5706ff938f6df1c0bfd3025334b4e8
Arika Turing <ablenker@gmail.com>
2021-06-03 11:21:58 -0600

    Add sad path specs for create subscription

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :validate_params, only: :create
+
   def create
     subscription = Subscription.create(subscription_params)
     if subscription.save
       render json: SubscriptionSerializer.new(subscription), status: :created
     else
-      render json: { error: pet.errors.full_messages.to_sentence }, status: :bad_request
+      render json: { error: subscription.errors.full_messages.to_sentence }, status: :bad_request
     end
   end
 
@@ -13,4 +15,8 @@ class Api::V1::SubscriptionsController < ApplicationController
   def subscription_params
     params.permit(:customer_id, :title, :price, :status, :frequency)
   end
+
+  def validate_params
+    render json: { error: 'Must provide request body' }, status: :bad_request if request.body.read.blank?
+end
 end
--- spec/requests/create_subscription_spec.rb
+++ spec/requests/create_subscription_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Create a pet request' do
+  before :each do
+    @customer = create(:customer)
+  end
   describe 'happy path' do
-    before :each do
-      @customer = create(:customer)
-    end
 
     it 'can create a subscription for a user' do
       sub_params = { customer_id: @customer.id,
@@ -49,7 +49,7 @@ RSpec.describe 'Create a pet request' do
 
       new_sub = Subscription.last
       new_sub_json = JSON.parse(response.body, symbolize_names: true)
-      
+
       expect(response).to be_successful
       expect(response.status).to eq(201)
       expect(response.content_type).to eq("application/json")
@@ -66,6 +66,59 @@ RSpec.describe 'Create a pet request' do
       expect(new_sub_json[:data][:attributes][:price]).to eq(sub_params[:price])
       expect(new_sub_json[:data][:attributes][:status]).to eq('active')
       expect(new_sub_json[:data][:attributes][:frequency]).to eq('two_weeks')
+    end
+  end
+  describe 'sad path' do
+    it 'unsuccessful response without require attributes' do
+      sub_params = { customer_id: @customer.id,
+                     price: 15,
+                     status: :active,
+                     frequency: :two_weeks }
+      headers = {"CONTENT_TYPE" => "application/json",
+                 "ACCEPT"       => "application/json"}
+
+      post "/api/v1/customers/#{@customer.id}/subscriptions", headers: headers, params: sub_params.to_json
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      expect(Subscription.all.count).to eq(0)
+
+      bad_request_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(bad_request_json[:error]).to eq("Title can't be blank")
+    end
+    it 'unsuccessful response without body' do
+      headers = {"CONTENT_TYPE" => "application/json",
+                 "ACCEPT"       => "application/json"}
+
+      post "/api/v1/customers/#{@customer.id}/subscriptions", headers: headers
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      expect(Subscription.all.count).to eq(0)
+
+      bad_request_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(bad_request_json[:error]).to eq("Must provide request body")
+    end
+    it 'unsuccessful response when price is less than 0' do
+      sub_params = { customer_id: @customer.id,
+                     title: 'All the Tea',
+                     price: -1,
+                     status: :active,
+                     frequency: :two_weeks }
+      headers = {"CONTENT_TYPE" => "application/json",
+                 "ACCEPT"       => "application/json"}
+
+      post "/api/v1/customers/#{@customer.id}/subscriptions", headers: headers, params: sub_params.to_json
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      expect(Subscription.all.count).to eq(0)
+
+      bad_request_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(bad_request_json[:error]).to eq("Price must be greater than or equal to 0")
     end
   end
 end
