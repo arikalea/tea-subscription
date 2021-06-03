053dc7668abf45bee527eae4d77cc4f536140b64
Arika Turing <ablenker@gmail.com>
2021-06-03 10:19:10 -0600

    Add happy path for subscription spec

--- spec/api/v1/create_subscription_spec.rb
+++ spec/api/v1/create_subscription_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'Create a pet request' do
 
       new_sub = Subscription.last
       new_sub_json = JSON.parse(response.body, symbolize_names: true)
-      # require "pry";binding.pry
+
       expect(response).to be_successful
       expect(response.status).to eq(201)
       expect(response.content_type).to eq("application/json")
@@ -36,6 +36,36 @@ RSpec.describe 'Create a pet request' do
       expect(new_sub_json[:data][:attributes][:price]).to eq(sub_params[:price])
       expect(new_sub_json[:data][:attributes][:status]).to eq(sub_params[:status].to_s)
       expect(new_sub_json[:data][:attributes][:frequency]).to eq(sub_params[:frequency].to_s)
+    end
+
+    it 'can create a subscription for a user with default values' do
+      sub_params = { customer_id: @customer.id,
+                     title: 'All the Tea',
+                     price: 15 }
+      headers = {"CONTENT_TYPE" => "application/json",
+                 "ACCEPT"       => "application/json"}
+
+      post "/api/v1/customers/#{@customer.id}/subscriptions", headers: headers, params: sub_params.to_json
+
+      new_sub = Subscription.last
+      new_sub_json = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+      expect(response.content_type).to eq("application/json")
+      expect(new_sub_json).to be_a(Hash)
+      expect(new_sub_json).to have_key(:data)
+      expect(new_sub_json[:data]).to be_a(Hash)
+      expect(new_sub_json[:data].keys).to eq([:id, :type, :attributes])
+      expect(new_sub_json[:data][:id]).to be_a(String)
+      expect(new_sub_json[:data][:type]).to eq('subscription')
+      expect(new_sub_json[:data][:attributes].count).to eq(5)
+      expect(new_sub_json[:data][:attributes].keys).to eq([:customer_id, :title, :price, :status, :frequency])
+      expect(new_sub_json[:data][:attributes][:customer_id]).to eq(@customer.id)
+      expect(new_sub_json[:data][:attributes][:title]).to eq(sub_params[:title])
+      expect(new_sub_json[:data][:attributes][:price]).to eq(sub_params[:price])
+      expect(new_sub_json[:data][:attributes][:status]).to eq('active')
+      expect(new_sub_json[:data][:attributes][:frequency]).to eq('two_weeks')
     end
   end
 end
