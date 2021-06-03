4d93f987e252cb4233dd85728381c886ebb51c5f
Arika Turing <ablenker@gmail.com>
2021-06-03 12:53:51 -0600

    Add happy path for update subscription

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::SubscriptionsController < ApplicationController
-  before_action :validate_params, only: :create
+  # before_action :validate_params, only: :create
 
   def index
     @customer = Customer.find(params[:customer_id])
@@ -19,13 +19,25 @@ class Api::V1::SubscriptionsController < ApplicationController
     end
   end
 
+  def update
+    @subscription = Subscription.find(params[:id])
+    if @subscription.update(update_sub_params)
+      render json: SubscriptionSerializer.new(@subscription), status: :ok
+    else
+      render json: { error: subscription.errors.full_messages.to_sentence }, status: :bad_request
+    end
+  end
+
   private
 
   def subscription_params
     params.permit(:customer_id, :title, :price, :status, :frequency)
   end
 
-  def validate_params
-    render json: { error: 'Must provide request body' }, status: :bad_request if request.body.read.blank?
+  def update_sub_params
+    params.permit(:status, :frequency)
   end
+  # def validate_params
+  #   render json: { error: 'Must provide request body' }, status: :bad_request if request.body.read.blank?
+  # end
 end
---
+++ spec/requests/update_subscription_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Update subscription request' do
+  before :each do
+    @customer = create(:customer)
+    @subscription = create(:subscription, customer: @customer)
+  end
+
+  describe 'happy path' do
+    it 'returns updated subscription' do
+      put "/api/v1/customers/#{@customer.id}/subscriptions/#{@subscription.id}?status=cancelled"
+
+      subscription_json = JSON.parse(response.body, symbolize_names: true)
+      # require "pry";binding.pry
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(subscription_json).to have_key(:data)
+      expect(subscription_json[:data]).to be_a(Hash)
+      expect(subscription_json[:data].keys).to eq([:id, :type, :attributes])
+      expect(subscription_json[:data][:id]).to be_a(String)
+      expect(subscription_json[:data][:type]).to eq('subscription')
+      expect(subscription_json[:data][:attributes].count).to eq(5)
+      expect(subscription_json[:data][:attributes].keys).to eq([:customer_id, :title, :price, :status, :frequency])
+      expect(subscription_json[:data][:attributes][:customer_id]).to eq(@customer.id)
+      expect(subscription_json[:data][:attributes][:title]).to eq(@subscription[:title])
+      expect(subscription_json[:data][:attributes][:price]).to eq(@subscription[:price])
+      expect(subscription_json[:data][:attributes][:status]).to eq('cancelled')
+      expect(subscription_json[:data][:attributes][:frequency]).to eq(@subscription[:frequency].to_s)
+    end
+  end
+end
