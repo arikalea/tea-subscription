86f2ba64d3b81d6ef56629e674ad04811ff2025e
Arika Turing <ablenker@gmail.com>
2021-06-06 16:49:00 -0600

    Add error handling to application controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::SubscriptionsController < ApplicationController
-  before_action :validate_params, only: :create
+  before_action :validate_params, only: [:create]
 
   def index
     @customer = Customer.find(params[:customer_id])
-    # if @customer
+    if @customer
       render json: SubscriptionSerializer.new(@customer.subscriptions), status: :ok
-    # else
-      # render json: { error: @customer.errors.full_messages.to_sentence }, status: :bad_request
-    # end
+    else
+      render json: { error: @customer.errors.full_messages.to_sentence }, status: :bad_request
+    end
   end
 
   def create
@@ -20,9 +20,10 @@ class Api::V1::SubscriptionsController < ApplicationController
   end
 
   def update
-    @subscription = Subscription.find(params[:id])
-    if @subscription.update(update_sub_params)
-      render json: SubscriptionSerializer.new(@subscription), status: :ok
+    subscription = Subscription.find(params[:id])
+    subscription.status = update_sub_params[:status]
+    if subscription.save!
+      render json: SubscriptionSerializer.new(subscription), status: :ok
     else
       render json: { error: subscription.errors.full_messages.to_sentence }, status: :bad_request
     end
@@ -37,7 +38,7 @@ class Api::V1::SubscriptionsController < ApplicationController
   def update_sub_params
     params.permit(:status, :frequency)
   end
-  
+
   def validate_params
     render json: { error: 'Must provide request body' }, status: :bad_request if request.body.read.blank?
   end
